bf6e26af55b4b5b42af05c8f99a616205f268a8d
Leo-Calsavara <calsavara2511@gmail.com>
2023-02-05 20:47:39 -0300

    funcoes de calculo prontas (sem testes)

---
+++ lagrange.py
@@ -0,0 +1,24 @@
+class ponto:
+    def __init__(self, ponto_x, ponto_y):
+        self.x = ponto_x
+        self.y = ponto_y
+
+    def __str__(self):
+        return "Seu ponto é {}, {}".format(self.x, self.y)
+
+
+def interpol(x_tenho, ponto_A, ponto_B, ponto_C):
+    L0 = ((x_tenho - ponto_B.x) * (x_tenho - ponto_C.x))/((ponto_A.x - ponto_B.x)*(ponto_A.x - ponto_C.x))
+    L1 = ((x_tenho - ponto_A.x) * (x_tenho - ponto_C.x))/((ponto_B.x - ponto_A.x)*(ponto_B.x - ponto_C.x))
+    L2 = ((x_tenho - ponto_A.x) * (x_tenho - ponto_B.x))/((ponto_C.x - ponto_A.x)*(ponto_C.x - ponto_B.x))
+    y_queria = (ponto_A.y*L0) + (ponto_B.y*L1) + (ponto_C.y*L2)   
+    return y_queria
+
+print("Favor entrar com os três pontos conhecidos:\n")
+A = ponto(int(input("digite o elemento X do ponto A: ")), int(input("digite o elemento Y do ponto A: ")))
+B = ponto(int(input("digite o elemento X do ponto B: ")), int(input("digite o elemento Y do ponto B: ")))
+C = ponto(int(input("digite o elemento X do ponto C: ")), int(input("digite o elemento Y do ponto C: ")))
+D = int(input("Entre com o X para o qual deseja descobir o y: "))
+y = interpol(D, A, B, C)
+seu_ponto = ponto(D,y)
+print("Seu y = " + seu_ponto.y)
